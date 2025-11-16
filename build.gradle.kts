e4410dab32de9f97bf15f47492dab5063ca15924
MHS <muhamad.salman2001@gmail.com>
2025-11-16 17:07:35 +0100

    fix: added config files for linters

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,6 +48,7 @@ tasks.named<Jar>("jar") {
 
 checkstyle {
     toolVersion = "10.15.0"	
+	config = resources.text.fromUri("https://raw.githubusercontent.com/checkstyle/checkstyle/master/src/main/resources/google_checks.xml")
 }
 
 pmd {
